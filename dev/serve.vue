977c108f334b46e9ecfd9d4930511f167f1c1540
sphirye <principe2366@gmail.com>
2021-09-20 00:18:59 -0400

    Component created

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+import { VueHexagonal } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueHexagonal,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <VueHexagonal img="https://static.wikia.nocookie.net/cookierun/images/c/c1/Starfruit_Cookie.png"/>
+  </div>
+</template>
---
+++ src/lib-components/VueHexagonal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="`vue-hexagon-container ${ outlined != undefined ? 'outlined' : '' }`">
+    <div :class="`vue-hexagon`" :style="`width: ${width}; height: ${height}; ${ img != undefined ? 'background-image: url('+img+');' : '' }`">
+      <span>{{text}}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component({ name: 'VueHexagonal' })
+export default class VueHexagonal extends Vue {
+
+  @Prop() readonly text: string | undefined
+  @Prop() readonly img: string | undefined
+  @Prop({ default: '50px' }) readonly width: string | undefined
+  @Prop({ default: '50px' }) readonly height: string | undefined
+  @Prop({ default: '50px' }) readonly transparent: string | undefined
+  @Prop() readonly outlined?: any | undefined
+
+}
+</script>
+
+<style>
+div.vue-hexagon-container {
+  display: inline-block;
+  -webkit-clip-path: polygon(50% 0, 100% 38%, 81% 100%, 19% 100%, 0 38%);
+  clip-path: polygon(50% 0, 100% 38%, 81% 100%, 19% 100%, 0 38%);
+}
+
+.vue-hexagon-container .vue-hexagon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(206, 206, 206);
+
+  background-size: cover;
+  background-position: center;
+
+  -webkit-clip-path: polygon(50% 0, 100% 38%, 81% 100%, 19% 100%, 0 38%);
+  clip-path: polygon(50% 0, 100% 38%, 81% 100%, 19% 100%, 0 38%);
+  margin: 3px;
+}
+
+.outlined {
+  background-color: black;
+}
+
+</style>
